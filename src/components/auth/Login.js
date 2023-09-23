630f917d5d703ce8ef4c2ac8ab6ffc82e3c9baf0
siddhant-0707 <chauhanjoy10@gmail.com>
2023-09-23 21:48:03 +0530

    login, signup add

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
-const Login = () => {
+import Navbar from "../navbar/Navbar";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginSuccess, setLoginSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const options = {
+      method: "POST",
+      url: "https://greenovate-server.vercel.app/api/auth/login",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        email: email,
+        password: password,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        if (response.status === 200) {
+          setLoginSuccess(true);
+          localStorage.setItem("token", response.data.token);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <Navbar />
+      <h2>Login</h2>
+      {loginSuccess ? (
+        <div>
+          <p>Successful login!</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default Login;
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
-const Signup = () => {
+import Navbar from "../navbar/Navbar";
+
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [orgName, setOrgName] = useState("");
+  const [signupSuccess, setSignupSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const options = {
+      method: "POST",
+      url: "https://greenovate-server.vercel.app/api/auth/signup",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        email: email,
+        password: password,
+        organizationName: orgName,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        if (response.status === 200) {
+          setSignupSuccess(true);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
   return (
-    <div>Signup</div>
-  )
+    <div>
+      <Navbar />
+      <h2>Sign Up</h2>
+      {signupSuccess ? (
+        <div>
+          <p>Signup successful! You can now log in.</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Organization Name:</label>
+            <input
+              type="text"
+              value={orgName}
+              onChange={(e) => setOrgName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Sign Up</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default Signup;
